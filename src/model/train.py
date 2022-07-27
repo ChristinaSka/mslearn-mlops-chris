d6e228f0a63d5ebc482a7f85608c9981e29a6569
Christina <christina.skarpathiotaki@microsoft.com>
2022-07-27 19:41:33 +0200

    modifying the linting with model variable

--- src/model/train.py
+++ src/model/train.py
@@ -21,7 +21,7 @@ def main(args):
     X_train, X_test, y_train, y_test = process_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 def get_csvs_df(path):
     if not os.path.exists(path):
